84f02e3ec2859ff3f676937dfb3d7b77f62051eb
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-08-08 12:38:52 +0900

    first_testcase

---
+++ src/main/java/com/back/App.java
@@ -0,0 +1,8 @@
+package com.back;
+
+public class App {
+    public void run(){
+        System.out.println("시스템 실행");
+    }
+}
+//App이 잘 작동하는 지 테스트
---
+++ src/test/java/com/back/AppTest.java
@@ -0,0 +1,21 @@
+package com.back;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    //테스트 케이스 1
+    @Test
+    @DisplayName("첫번째 테스트")
+    void t1(){
+        System.out.println("테스트1");
+    }
+
+    //테스트 케이스 1
+    @Test
+    @DisplayName("두번째 테스트")
+    void t2(){
+        System.out.println("테스트2");
+    }
+}
